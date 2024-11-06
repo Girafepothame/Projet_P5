b3d5d65ee6d864586b0fc9b995767eedbcbbc56b
Thomas Csn <thomas.cosson@etu.univ-st-etienne.fr>
2024-11-06 16:55:24 +0100

    add boss class

---
+++ scripts/classes/boss.js
@@ -0,0 +1,25 @@
+class boss{
+    constructor(size, x, y, tabEnemy, hp, deathScore, speed, difficulty){
+      this.tabEnemy = tabEnemy;
+      this.hp = hp;
+      this.deathScore = deathScore
+      this.size = size;
+      this.pos = createVector(x,y);
+      this.speed = speed;
+      this.difficulty = difficulty;
+      
+    }
+    
+    
+    draw(){
+      push()
+      rectMode(CENTER)
+      noStroke()
+      fill(255,132,132)
+      rect(this.pos.x, this.pos.y, this.size)
+      
+      pop()
+    }
+    
+    
+  }
